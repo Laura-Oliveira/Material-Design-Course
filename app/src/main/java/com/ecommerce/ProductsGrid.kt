adc9b173a2e63b48365ae8bdd21dc04a5f6d5bfd
Laura <laura@Mac-Puma.local>
2025-01-16 19:01:22 -0300

    [Best Code Practices][Design Patterns] Creation Design Pattern to follow best programming practices with patterns on the styles implemented on the screens | Code refactoring

--- app/src/main/java/com/ecommerce/ProductsGrid.kt
+++ app/src/main/java/com/ecommerce/ProductsGrid.kt
@@ -36,7 +36,8 @@ class ProductsGrid : AppCompatActivity()
         val products = listOf(
             Product(title = "Product 1", price = "$10.00", url = "https://www.kapwing.com/studio/editor"),
             Product(title = "Product 2", price = "$20.00", url = "https%3A%2F%2Fpreview.redd.it%2Fi-keep-seeing-this-angry-cat-meme-does-anyone-know-what-v0-n9p8aheg9jw91.jpg%3Fwidth%3D1080%26crop%3Dsmart%26auto%3Dwebp%26s%3Daf0ff55ee92c8479c148d47e34d285633b98f76b"),
-            Product(title = "Product 3", price = "$30.00", url = "https://jp.pinterest.com/pin/649081365075452682/")
+            Product(title = "Product 3", price = "$30.00", url = "https://jp.pinterest.com/pin/649081365075452682/"),
+            Product(title = "Product 4", price = "$40.00", url = "https://jp.pinterest.com/pin/649081365075452682/")
         )
 
 //        val products = listOf(
